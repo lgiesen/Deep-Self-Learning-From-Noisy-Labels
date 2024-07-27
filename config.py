96adde6bc7c226a9af185124b62c5ace1a3512e0
Leo Giesen <contact@leogiesen.de>
2024-07-27 10:10:18 +0200

    :wrench: add parameters

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
 """
 This file comprises global variables, which are used by multiple files.
 """
+# dataset variables
 dataset_root = '/Volumes/Festplatte/MATIML/data/clothing1M/'
 dataset_img = f'{dataset_root}images/'
 dataset_masks = f'{dataset_root}annotations/'
@@ -9,8 +10,8 @@ dataset_train_path = f'{dataset_root}train_dataset.csv'
 dataset_val_path = f'{dataset_root}val_dataset.csv'
 dataset_test_path = f'{dataset_root}test_dataset.csv'
 class_names = [line.strip() for line in open(f'{dataset_masks}category_names_eng.txt')]
-
 batch_size=128
+# model variables
 num_epochs=15
 momentum=0.9
 weight_decay=5e-3
@@ -19,3 +20,7 @@ lr=0.002
 # decrease lr by 10 every 5 epochs
 step_size=5
 gamma=0.1
+# weight factor alpha to balance the loss function for the original and corrected data
+alpha=0.5
+randomly_samples_img_size=1280
+num_prototypes=8
