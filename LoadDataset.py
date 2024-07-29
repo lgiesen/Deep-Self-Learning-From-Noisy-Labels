58292f24bceff0230cb5051a1ee58a416dc045d1
Leo Giesen <contact@leogiesen.de>
2024-07-29 14:28:06 +0200

    :fire: undo the data loading for parts of the images

--- LoadDataset.py
+++ LoadDataset.py
@@ -1,7 +1,7 @@
 import os
 
 import pandas as pd
-from PIL import Image, UnidentifiedImageError
+from PIL import Image
 from torch.utils.data import Dataset
 
 from config import dataset_root
@@ -9,26 +9,21 @@ from config import dataset_root
 
 class CustomImageDataset(Dataset):
     def __init__(self, file_path, folder_path=dataset_root, transform=None):
-        self.image_labels = pd.read_csv(file_path, sep=' ', header=None, names=['image_path', 'label'])
+        self.image_labels = pd.read_csv(file_path, 
+                                        sep=' ', 
+                                        header=None, 
+                                        names=['image_path', 'label'])
         self.folder_path = folder_path
         self.transform = transform
-        self.total_images = len(self.image_labels)
-        self.missing_images = 0
 
     def __len__(self):
-        return self.total_images
+        return len(self.image_labels)
 
     def __getitem__(self, idx):
         img_rel_path = self.image_labels.iloc[idx, 0]
         img_path = os.path.join(self.folder_path, img_rel_path)
-        try:
-            image = Image.open(img_path).convert('RGB')
-        except (FileNotFoundError, UnidentifiedImageError):
-            self.missing_images += 1
-            # Return a placeholder value to avoid crashing
-            return None, None, idx
-
+        image = Image.open(img_path).convert('RGB')
         label = int(self.image_labels.iloc[idx, 1])
         if self.transform:
             image = self.transform(image)
-        return image, label, idx  # Include index for tracking
+        return image, label
