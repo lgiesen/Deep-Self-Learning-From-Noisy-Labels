8b74192711ed48f44d095d373e6d0c0172651cac
Leo Giesen <contact@leogiesen.de>
2024-07-29 13:26:39 +0200

    :bug: adjust dataset loading for training without all images (too large to upload all to GDrive)

--- LoadDataset.py
+++ LoadDataset.py
@@ -9,21 +9,25 @@ from config import dataset_root
 
 class CustomImageDataset(Dataset):
     def __init__(self, file_path, folder_path=dataset_root, transform=None):
-        self.image_labels = pd.read_csv(file_path, 
-                                        sep=' ', 
-                                        header=None, 
-                                        names=['image_path', 'label'])
+        self.image_labels = pd.read_csv(file_path, sep=' ', header=None, names=['image_path', 'label'])
         self.folder_path = folder_path
         self.transform = transform
+        self.total_images = len(self.image_labels)
+        self.missing_images = 0
 
     def __len__(self):
-        return len(self.image_labels)
+        return self.total_images
 
     def __getitem__(self, idx):
         img_rel_path = self.image_labels.iloc[idx, 0]
         img_path = os.path.join(self.folder_path, img_rel_path)
-        image = Image.open(img_path).convert('RGB')
+        try:
+            image = Image.open(img_path).convert('RGB')
+        except FileNotFoundError:
+            self.missing_images += 1
+            return None  # Returning None for missing images
+
         label = int(self.image_labels.iloc[idx, 1])
         if self.transform:
             image = self.transform(image)
-        return image, label
+        return image, label, idx  # Include index for tracking
